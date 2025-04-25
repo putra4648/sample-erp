4d24baf3fffc516c62b888df3c965026e20a7dd5
putra4648 <wakwaujaim@gmail.com>
2025-04-25 20:37:07 +0700

    first commit

---
+++ src/main/java/id/putra/my_erp/MyErpApplication.java
@@ -0,0 +1,13 @@
+package id.putra.my_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/putra/my_erp/MyErpApplicationTests.java
@@ -0,0 +1,13 @@
+package id.putra.my_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
